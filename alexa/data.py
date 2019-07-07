73a23173f02c610b7034fb1ce4f11841c7e6ca01
xo28122000 <46950951+xo28122000@users.noreply.github.com>
2019-07-07 09:43:21 -0700

    Update data.py

--- alexa/data.py
+++ alexa/data.py
@@ -90,8 +90,3 @@ GENERIC_REPROMPT = _("What can I help you with?")
 #     ],
 # }
 
-MY_API = {
-    "host": "https://query.yahooapis.com",
-    "port": 443,
-    "path": "/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{city}%2C%20{state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
-}
